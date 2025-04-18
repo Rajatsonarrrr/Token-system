657cb8648e29a8bbacd7fd1fad944c1de601dc4f
Phelix-business <acmagrade.cto@gmail.com>
2025-04-18 10:54:39 +0530

    delted love1.html

--- public/display.js
+++
@@ -1,9 +0,0 @@
-const socket = io('http://localhost:3000'); // Use your IP on LAN
-
-socket.on('updateRoomNumbers', ({ room1, room2 }) => {
-  const room1Tokens = document.querySelectorAll('#room1 span');
-  const room2Tokens = document.querySelectorAll('#room2 span');
-
-  room1.forEach((num, i) => room1Tokens[i].textContent = num);
-  room2.forEach((num, i) => room2Tokens[i].textContent = num);
-});
