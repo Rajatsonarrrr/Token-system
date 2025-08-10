217973a70ac3d7ed579f38a04317817facde01db
Rajatsonarrrr <sonarrajat47@gmail.com>
2025-08-11 03:29:57 +0530

    pushed

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve control.html as the default page at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'control.html'));
+});
+
+// Socket.io communication
 io.on('connection', (socket) => {
   console.log('A user connected');
 
@@ -18,6 +25,7 @@ io.on('connection', (socket) => {
   });
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
